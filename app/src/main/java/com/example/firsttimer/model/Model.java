118698f000a89f1258de704f7c74befa59091026
Madictian <fredrikbille@hotmail.com>
2021-02-02 20:01:15 +0100

    MainActivity moved to newly created view, Model package and class is created

--- app/src/main/java/com/example/firsttimer/model/Model.java
+++ app/src/main/java/com/example/firsttimer/model/Model.java
@@ -1,4 +1,19 @@
 package com.example.firsttimer.model;
 
-public class Model {
-}
+import java.util.Observable;
+
+    public class Model extends Observable {
+
+        private String data = "";
+
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+            super.setChanged();
+            super.notifyObservers();
+        }
+    }
--- app/src/main/java/com/example/firsttimer/view/MainActivity.java
+++ app/src/main/java/com/example/firsttimer/view/MainActivity.java
@@ -8,21 +8,53 @@ import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.firsttimer.R;
+import com.example.firsttimer.model.Model;
+
+import java.util.Observable;
+import java.util.Observer;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+        private Model model = new Model();
 
 
-    @Override
+
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        observeModel(model);
+
+        TextView textView = (TextView) findViewById(R.id.textView3);
+        textView.setText(model.getData());
+
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName2);
+        editText.setText(model.getData());
+
     }
 
+    private void observeModel(Model model){
+       model.addObserver(new Observer(){
+
+           @Override
+           public void update(Observable o, Object arg) {
+               if (o instanceof Model){
+                   String data = ((Model) o).getData();
+                   TextView textView = (TextView) findViewById(R.id.textView3);
+                   textView.setText(data);
+               }
+           }
+       });
+    }
+
+
     public void setText(View view){
-        TextView textView = (TextView) findViewById(R.id.textView3);
+
         EditText editText = (EditText) findViewById(R.id.editTextTextPersonName2);
-        textView.setText(editText.getEditableText());
+
+        model.setData(editText.getEditableText().toString());
     }
 
 }
