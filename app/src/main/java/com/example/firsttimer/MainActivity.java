f256aef16cdfb4714f6c4bd2f75238dea26fb1cd
Madictian <fredrikbille@hotmail.com>
2021-02-02 07:46:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/firsttimer/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.firsttimer;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void setText(View view){
+        TextView textView = (TextView) findViewById(R.id.textView3);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName2);
+        textView.setText(editText.getEditableText());
+    }
+
+}
